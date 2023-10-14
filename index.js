bf5c2d2524b108db624c16854dbde1cc1cc58c0b
john@home <johninquest@gmail.com>
2023-10-14 17:33:37 +0200

    playing with api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const usersRouter = require("./routes/user");
-const defaultRouter = require("./routes/default");
+const userRouter = require("./routes/user");
+const defaultRouter = require("./routes/api");
 // const cors = require('cors')
 
 const app = express();
 // app.use(express.cors());
 
 // Mount the users router
-app.use("/user", usersRouter);
-app.use("/default", defaultRouter);
+app.use("/user", userRouter);
+app.use("/api", defaultRouter);
 
 // Parse JSON bodies
 // app.use(express.json());
@@ -16,7 +16,7 @@ app.use("/default", defaultRouter);
 // app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.redirect("/default");
+  res.redirect("/api");
 });
 
 app.listen(3000, () => {
